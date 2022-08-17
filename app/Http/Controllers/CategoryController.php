fcc49049aa9a9752821c5be1fd3745ef911e98ea
duhabduh <muhamad.abduh.muh@gmail.com>
2022-08-17 14:16:57 +0700

    feature-crud-categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $limit = $request->query('page_size', 10);
+        $categories = Category::paginate($limit);
+        return response()->json($categories);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:categories',
+            'enable' => 'required'
+        ]);
+
+        $category = new Category();
+        $category->name = $request['name'];
+        $category->enable = $request['enable'];
+        $category->save();
+
+        return response()->json($category, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return response()->json($category->load('products'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $validated = $request->validate([
+            'name' => 'required|unique:categories',
+            'enable' => 'required'
+        ]);
+
+        $category->name = $request['name'];
+        $category->enable = $request['enable'];
+        $category->save();
+
+        return response()->json($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json(['message' => 'deleted']);
+    }
+}
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    public function produts()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
